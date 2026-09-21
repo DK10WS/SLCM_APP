3bdf7ad6f01d2353dce2db9ba2f5db6aadf39e7b
Dhruv Kunzru <dhruvkunzru@Gmail.com>
2026-09-21 17:46:35 +0530

    Send same-origin headers so SLCM accepts logins SLCM rejects login posts without Referer and Sec-Fetch-Site

--- lib/utils/util.dart
+++ lib/utils/util.dart
@@ -7,6 +7,8 @@ final headers = {
   "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
   "Content-Type": "application/x-www-form-urlencoded",
+  "Sec-Fetch-Site": "same-origin",
+  "Referer": "https://mujslcm.jaipur.manipal.edu",
 };
 
 final dio = Dio();
